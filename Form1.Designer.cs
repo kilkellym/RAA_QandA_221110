bda8fb470abb4d6ce1d7af99c4e3e10652d29694
kilkellym <michael@archsmarter.com>
2022-12-08 17:44:48 -0500

    minor UI fixes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,28 +38,29 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 348);
+            this.comboBox1.Location = new System.Drawing.Point(44, 417);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(520, 39);
+            this.comboBox1.Size = new System.Drawing.Size(527, 39);
             this.comboBox1.TabIndex = 0;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(12, 467);
+            this.comboBox2.Location = new System.Drawing.Point(44, 536);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(520, 39);
+            this.comboBox2.Size = new System.Drawing.Size(527, 39);
             this.comboBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 297);
+            this.label1.Location = new System.Drawing.Point(44, 373);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(319, 32);
             this.label1.TabIndex = 2;
@@ -68,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 419);
+            this.label2.Location = new System.Drawing.Point(44, 493);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(303, 32);
             this.label2.TabIndex = 3;
@@ -76,11 +77,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(531, 12);
+            this.button1.Location = new System.Drawing.Point(580, 81);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 53);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Select";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -88,7 +89,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 31;
-            this.listBox1.Location = new System.Drawing.Point(18, 12);
+            this.listBox1.Location = new System.Drawing.Point(50, 81);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(507, 159);
             this.listBox1.TabIndex = 6;
@@ -96,7 +97,7 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button2.Location = new System.Drawing.Point(531, 532);
+            this.button2.Location = new System.Drawing.Point(563, 601);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(114, 50);
             this.button2.TabIndex = 7;
@@ -106,7 +107,7 @@
             // button4
             // 
             this.button4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button4.Location = new System.Drawing.Point(674, 532);
+            this.button4.Location = new System.Drawing.Point(706, 601);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(114, 50);
             this.button4.TabIndex = 9;
@@ -116,7 +117,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 191);
+            this.label3.Location = new System.Drawing.Point(50, 269);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(303, 32);
             this.label3.TabIndex = 11;
@@ -124,16 +125,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 245);
+            this.textBox1.Location = new System.Drawing.Point(50, 314);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(521, 38);
             this.textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(633, 80);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Select Family Files";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 604);
+            this.ClientSize = new System.Drawing.Size(842, 669);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button4);
@@ -163,5 +174,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
